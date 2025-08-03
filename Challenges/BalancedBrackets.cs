bb770772854b0da7963145bf9b79f711ad63fce4
Azam <bajzaevazam@gmail.com>
2025-08-03 15:57:38 +0500

    Add files via upload

--- Challenges/BalancedBrackets.cs
+++ Challenges/BalancedBrackets.cs
@@ -1,38 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class BalancedBrackets
+namespace Main
 {
-    public static bool IsBalanced(string s)
+    internal class BalancedBrackets
     {
-        Stack<char> stack = new Stack<char>();
-
-        foreach (char c in s)
-        {
-           
-            if (c == '(' || c == '{' || c == '[')
-            {
-                stack.Push(c);
-            }
-          
-            else if (c == ')' || c == '}' || c == ']')
-            {
-                if (stack.Count == 0) return false;
-
-                char top = stack.Pop();
-
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '['))
-                {
-                    return false;
-                }
-            }
-        }
-
-       
-        return stack.Count == 0;
     }
-
 }
-
