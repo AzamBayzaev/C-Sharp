fd6c06ad530c6e53855c8b68dcafc3df20bb68cb
Azam <bajzaevazam@gmail.com>
2025-08-03 15:57:56 +0500

    Update BalancedBrackets.cs

--- Challenges/BalancedBrackets.cs
+++ Challenges/BalancedBrackets.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Main
+class MaxCircularSubarraySu
 {
-    internal class BalancedBrackets
+    private int[] nums;
+
+    public MaxCircularSubarraySu(int[] nums)
     {
+        if (nums == null || nums.Length == 0)
+            throw new ArgumentException("Массив не может быть пустым.");
+
+        this.nums = nums;
+    }
+
+    public int Calculate()
+    {
+        int totalSum = 0;
+        int maxSum = nums[0];
+        int curMax = 0;
+        int minSum = nums[0];
+        int curMin = 0;
+
+        foreach (int num in nums)
+        {
+
+            curMax = Math.Max(num, curMax + num);
+            maxSum = Math.Max(maxSum, curMax);
+
+
+            curMin = Math.Min(num, curMin + num);
+            minSum = Math.Min(minSum, curMin);
+
+            totalSum += num;
+        }
+
+
+        if (maxSum < 0)
+            return maxSum;
+
+        return Math.Max(maxSum, totalSum - minSum);
     }
 }
+
+
+
