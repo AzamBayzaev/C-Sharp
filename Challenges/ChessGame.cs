4a41b6ae96a77ff45d076015d6ea716891a16fd5
Azam <bajzaevazam@gmail.com>
2025-10-27 18:17:17 +0500

    Add files via upload

---
+++ Challenges/ChessGame.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Main
+{
+    [Flags]
+    enum MoveType
+    {
+        None = 0,
+        Diagonal = 1,   // ход по диагонали
+        Straight = 2,   // ход по прямой линии (вверх, вниз, влево, вправо)
+        LShape = 4      // ход коня "буквой Г"
+    }
+
+    enum ChessPiece
+    {
+        King = 1,
+        Queen,
+        Rook,
+        Bishop,
+        Knight,
+        Pawn
+    }
+
+    internal class ChessGame
+    {
+        public static void Run()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Выберите фигуру:");
+            foreach (ChessPiece piece in Enum.GetValues(typeof(ChessPiece)))
+            {
+                Console.WriteLine($"{(int)piece}. {piece}");
+            }
+
+            Console.Write("Введите номер фигуры: ");
+            if (!int.TryParse(Console.ReadLine(), out int choice) || !Enum.IsDefined(typeof(ChessPiece), choice))
+            {
+                Console.WriteLine("Неверный ввод!");
+                return;
+            }
+
+            ChessPiece selected = (ChessPiece)choice;
+            MoveType moves = GetMoveType(selected);
+
+            Console.WriteLine($"\nВы выбрали: {selected}");
+            Console.WriteLine("Возможные типы ходов:");
+
+            if ((moves & MoveType.Diagonal) != 0) Console.WriteLine("- По диагонали");
+            if ((moves & MoveType.Straight) != 0) Console.WriteLine("- По прямой линии");
+            if ((moves & MoveType.LShape) != 0) Console.WriteLine("- Ход конем (буква Г)");
+
+            if (moves == MoveType.None) Console.WriteLine("- Нет доступных ходов");
+        }
+
+        static MoveType GetMoveType(ChessPiece piece)
+        {
+            return piece switch
+            {
+                ChessPiece.King => MoveType.Diagonal | MoveType.Straight,
+                ChessPiece.Queen => MoveType.Diagonal | MoveType.Straight,
+                ChessPiece.Rook => MoveType.Straight,
+                ChessPiece.Bishop => MoveType.Diagonal,
+                ChessPiece.Knight => MoveType.LShape,
+                ChessPiece.Pawn => MoveType.Straight,
+                _ => MoveType.None
+            };
+        }
+    }
+}
