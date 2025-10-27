4cd8eb88eb1c60ce206cf889cf0b1e4a6e916520
Azam <bajzaevazam@gmail.com>
2025-10-27 20:01:25 +0500

    Add files via upload

---
+++ Challenges/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Factorial
+    {
+        public static void Run()
+        {
+            Console.Write("Введите число: ");
+            if (!int.TryParse(Console.ReadLine(), out int n) || n < 0)
+            {
+                Console.WriteLine("Неверный ввод! Введите неотрицательное число.");
+                return;
+            }
+
+            long fact = 1;
+            for (int i = 2; i <= n; i++)
+                fact *= i;
+
+            Console.WriteLine($"Факториал числа {n} = {fact}");
+        }
+    }
+}
