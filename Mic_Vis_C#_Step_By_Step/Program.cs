2d6cc7eb50346e326c57aaf0baf37140a2cff746
Azam <bajzaevazam@gmail.com>
2025-10-31 20:25:43 +0500

    Add files via upload

---
+++ Mic_Vis_C#_Step_By_Step/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(userInput);
+                Console.WriteLine($"You entered a number: {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Invalid number entered. Please enter a valid number.");
+            }
+
+        }
+    }
+}
