2b1a571dec4e6af819e143e583f10c8287ee5f27
Azam <bajzaevazam@gmail.com>
2025-11-03 21:31:38 +0500

    Update Dairy.cs

--- Challenges/Dairy.cs
+++ Challenges/Dairy.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Main
 {
@@ -111,6 +107,7 @@ namespace Main
             }
         }
     }
+
 
 
 
