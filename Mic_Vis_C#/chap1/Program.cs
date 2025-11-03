2df1b6e03f2d13817898e574a2e051ceb590e48e
Azam <bajzaevazam@gmail.com>
2025-11-03 21:14:17 +0500

    Add files via upload

---
+++ Mic_Vis_C#/chap1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(userInput);
+                Console.WriteLine($"You entered number: {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Invalid number entered. Please enter a valid number.");
+            }
+        }
+    }
+}
