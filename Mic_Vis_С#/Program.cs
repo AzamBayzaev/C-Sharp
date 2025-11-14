f83d83896fee93407a703b4aafdf81988edd0122
Azam <bajzaevazam@gmail.com>
2025-11-14 19:51:46 +0500

    Add files via upload

---
+++ Mic_Vis_С#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Main
+{
+    class Animals
+    {
+        public string Name;
+        public Animals(string name) => Name = name;
+        public virtual void MakeSound()
+        {
+            Console.Write($"{Name} says: ");
+        }
+    }
+    class Cat : Animals
+    {
+        public Cat(string name) : base(name) {}
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("miy");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat = new Cat("Tima");
+            cat.MakeSound();
+
+            IntExtensions.IsEven(5);
+
+        }
+    }
+
+    static class IntExtensions
+    {     
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
