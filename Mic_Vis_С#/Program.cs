f5edf7a6345a1999db93edb0745f42dfcb1b1078
Azam <bajzaevazam@gmail.com>
2025-11-14 20:35:03 +0500

    Add files via upload

---
+++ Mic_Vis_С#/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Main
+{
+    interface IArray
+    {
+        double GetArray();
+    }
+    abstract class Shape
+    {
+        public abstract void GetInfo();
+    }
+    class Rectangle : Shape, IArray
+    {
+        private int Length;
+        private int Width;
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public double GetArray() => Length * Width;
+        public override void GetInfo() => Console.WriteLine($"Length: {Length}\nWidth: {Width}");
+    }
+    sealed class Circle : Shape, IArray
+    {
+        private int Radius;
+        public Circle(int radius) => Radius = radius;
+        public double GetArray() => Math.PI * Math.Pow(Radius, 2);
+        public override void GetInfo() => Console.WriteLine($"Radius: {Radius}");
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(5, 3);
+            rect.GetInfo();
+            Console.WriteLine($"Rectangle area: {rect.GetArray()}");
+
+            Circle circle = new Circle(4);
+            circle.GetInfo();
+            Console.WriteLine($"Circle area: {circle.GetArray()}");
+
+        }
+    }
+}
