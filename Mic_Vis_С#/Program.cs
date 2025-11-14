4c7df4eca0d1a2cb71aec63b591f97094330556c
Azam <bajzaevazam@gmail.com>
2025-11-14 19:15:40 +0500

    Add files via upload

---
+++ Mic_Vis_С#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Method(params int[] a)
+            {
+                foreach (int i in a)
+                {
+                    if (i % 2 == 0)
+                        Console.WriteLine($"Even {i}");
+                    else
+                        Console.WriteLine($"Odd {i}");
+                }
+            }
+
+            Method(2,3,4,6,8,9);
+
+            void Method2(params object[] paramList)
+            {
+                Console.WriteLine(paramList.ToString());
+
+            }
+            Method2(2, 3, 4, 6, 8, 9);
+        }
+    }
+}
