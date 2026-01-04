ed9471b09e347332459101a6abd365e3287fce86
Azam <bajzaevazam@gmail.com>
2026-01-04 13:56:38 +0500

    Add files via upload

---
+++ Mic_Vis_С#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        using (var Fs = new FileStream("File.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite))
+        {
+            byte[] buffer = new byte[100];
+            var ReadFs = Fs.Read(buffer, 0, 3);
+            Console.WriteLine(buffer);
+        }
+    }
+}
