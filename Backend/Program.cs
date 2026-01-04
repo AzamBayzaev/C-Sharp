c0b7bf86f7913188169074ac1f9421eaf9c37c9a
Azam <bajzaevazam@gmail.com>
2026-01-04 13:58:16 +0500

    Update Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -8,11 +8,6 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
 
-        using (var Fs = new FileStream("File.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite))
-        {
-            byte[] buffer = new byte[100];
-            var ReadFs = Fs.Read(buffer, 0, 3);
-            Console.WriteLine(buffer);
-        }
     }
 }
+
